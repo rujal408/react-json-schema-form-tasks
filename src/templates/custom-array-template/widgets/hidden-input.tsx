2cb1e95107f6e46601892b66522b148da8f238b1
rujal408 <rujal408@gmail.com>
2025-05-22 11:36:24 +0545

    add context

--- src/templates/custom-array-template/widgets/hidden-input.tsx
+++ src/templates/custom-array-template/widgets/hidden-input.tsx
@@ -1,27 +1,27 @@
 import type { RJSFSchema, WidgetProps } from "@rjsf/utils";
 import React, { useEffect, useRef } from "react";
 import { useCustomArrayItemContext } from "../provider";
-import eventBus from "../../../utils/Event";
+// import eventBus from "../../../utils/Event";
 
 const HiddenInput: React.FC<WidgetProps<any, RJSFSchema, any>> = (props) => {
   const {
-    index,
-    // control: { val },
+    // index,
+    control: { val },
   } = useCustomArrayItemContext();
-  // const prevValRef = useRef(val); // Track previous value of val
+  const prevValRef = useRef(val); // Track previous value of val
 
   useEffect(() => {
-    // if (prevValRef.current.length === 0 && val.length > 0) {
-    // Only trigger change when val goes from empty to non-empty
-    // props.onChange(true);
-    eventBus.on("call-" + index, (data) => {
-      console.log({ data });
+    if (prevValRef.current.length === 0 && val.length > 0) {
+      // Only trigger change when val goes from empty to non-empty
       props.onChange(true);
-    });
-    //
-    // }
-    // prevValRef.current = val; // Update previous value after effect runs
-  }, []);
+    }
+    prevValRef.current = val; // Update previous value after effect runs
+
+    // eventBus.on("call-" + index, (data) => {
+    //   console.log({ data });
+    //   props.onChange(true);
+    // });
+  }, [val]);
 
   return <input type="hidden" />;
 };
